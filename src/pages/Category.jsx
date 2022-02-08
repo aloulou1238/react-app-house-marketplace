f70e799d5e7605f79d6685805b5c1285cfe5bcb5
aloulou1238 <benhibaali@gmail.com>
2022-02-08 11:54:29 +0100

    second commit

--- src/pages/Category.jsx
+++ src/pages/Category.jsx
@@ -1,14 +1,17 @@
 import {useEffect, useState} from 'react'
+import {useParams} from 'react-router-dom'
 import {collection, getDocs, query, where, orderBy, limit, startAfter} from 'firebase/firestore'
 import {db} from '../firebase.config'
 import {toast} from 'react-toastify'
 import Spinner from '../components/Spinner'
 import ListingItem from '../components/ListingItem'
 
-function Offers() {
+function Category() {
   const [listings, setListings] = useState(null)
   const [loading, setLoading] = useState(true)
   const [lastFetchedListing, setLastFetchedListing] = useState(null)
+
+  const params = useParams()
 
   useEffect(() => {
     const fetchListings = async () => {
@@ -19,7 +22,7 @@ function Offers() {
         //Create a query
         const q = query(
           listingsRef, 
-          where('offer', '==', true), 
+          where('type', '==', params.categoryName), 
           orderBy('timestamp', 'desc'), 
           limit(10)
         )
@@ -43,7 +46,8 @@ function Offers() {
       }
     } 
     fetchListings()
-  }, [])
+  }, [params.categoryName])
+
 
   //Pagination / Load More
   const onFetchMoreListings = async () => {
@@ -54,7 +58,7 @@ function Offers() {
         //Create a query
         const q = query(
           listingsRef, 
-          where('offer', '==', true), 
+          where('type', '==', params.categoryName), 
           orderBy('timestamp', 'desc'), 
           startAfter(lastFetchedListing),
           limit(10)
@@ -77,12 +81,12 @@ function Offers() {
       } catch (error) {
         toast.error('Could not fetch listings')
       }
-    }
+    } 
 
   return <div className='category'>
     <header>
       <p className="pageHeader">
-        Offers
+        {params.categoryName === 'rent' ? 'Places for rent' : 'Places for sale'}
       </p>
     </header>
 
@@ -104,10 +108,9 @@ function Offers() {
         {lastFetchedListing && (
           <p className="loadMore" onClick={onFetchMoreListings}>Load More</p>
         )}
-
       </> ) : ( 
-        <p>There are no current offers</p>)} 
+        <p>No listings for {params.categoryName}</p>)} 
   </div>;
 }
 
-export default Offers;
+export default Category;
